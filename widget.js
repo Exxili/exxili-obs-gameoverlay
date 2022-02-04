4e2b56687ab32077393d12e02c01bae116b25fe0
Exxili <sirvaux@live.com>
2022-02-04 09:03:06 +0000

    added eslint

--- widget.js
+++ widget.js
@@ -1,6 +1,8 @@
-const { useQuasar } = Quasar
-const { ref } = Vue
+/* global Quasar */
+/* global Vue */
 
+const { useQuasar } = Quasar;
+const { ref } = Vue;
 
 /*
 Example kicking off the UI. Obviously, adapt this to your specific needs.
@@ -8,45 +10,42 @@ Assumes you have a <div id="q-app"></div> in your <body> above
 */
 const app = Vue.createApp({
   setup() {
-    const $q = useQuasar()
+    const $q = useQuasar();
 
-    const osTime = ref("")
+    const osTime = ref('');
 
     setInterval(() => {
       const dateTime = new Date();
       osTime.value = dateTime.toLocaleTimeString();
     }, 1000);
 
-
-
     window.addEventListener('onEventReceived', (obj) => {
-      if (obj.detail.listener === "follower-latest") {
+      if (obj.detail.listener === 'follower-latest') {
         $q.notify({
           group: false,
           message: `Hey this worked ${obj.detail.event.name}`,
-          position: "top-right"
-        })
+          position: 'top-right',
+        });
       }
     });
 
     return {
-      osTime
-    }
-  }
-})
-
+      osTime,
+    };
+  },
+});
 
 // Clock Component
-const clock = app.component('clock', {
+app.component('clock', {
   data() {
     return {
 
-    }
+    };
   },
   template: `
     <h1>test</h1>
-  `
-})
+  `,
+});
 
-app.use(Quasar, { config: {} })
-app.mount('#q-app')
+app.use(Quasar, { config: {} });
+app.mount('#q-app');
